fb376749ecea3544e2171cd7d39c617f89b63c38
eciras <92270701+eciras@users.noreply.github.com>
2022-08-05 13:13:59 +0300

    Added datePicker

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -1,13 +1,39 @@
 import React from "react";
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css'; 
+import { DateRange } from 'react-date-range';
+import { useState } from 'react';
 
-function Card({company}) {
+
+function Card({ company }) {
+
+
+    const [state, setState] = useState([
+        {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+        }
+    ]);
 
 
     return (
         <div className="card">
-            <img src={company.logo}></img>
-            <label>{company.name}</label><br />
+            <label>{company.name}</label>
+            <label>{company.country}</label>
+            <label>{company.currency}</label>
             <label>{company.weburl}</label>
+
+
+            <DateRange className="datePicker"
+                editableDateInputs={true}
+                onChange={item => setState([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={state}
+            />
+
+
+
         </div>
     )
 }
--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -4,30 +4,39 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function isSearchValid(search) {
-    return /^[a-zA-Z]{1,35}$/g.test(search)
+    return /^[a-zA-Z ]{1,35}$/g.test(search)
 }
 
 function MainPage() {
 
     const [search, setSearch] = useState('');
     const [company, setCompany] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+  
+
+    // const handleClick = () => {
+    //     if (!isSearchValid(search) === true) {
+    //     setErrorMessage("Example error message!")
+    //   }
+    // }
+
     const searchCompany = (e) => {
+        if (!isSearchValid(search) === true) {
+            setErrorMessage("Example error message!");
+                
+            
 
-        if (!isSearchValid(search) == true) {
-            alert("bad")
         } else {
-        
-        
-            axios.get('https://finnhub.io/api/v1/stock/profile2?token=cbkj62iad3i8o8768nog&symbol='+search)
-            .then(res => { 
-                setCompany(res.data)
-                console.log(res.data)
-            })
-            .catch(err => console.log(err))
-
+            axios.get('https://finnhub.io/api/v1/stock/profile2?token=cbkj62iad3i8o8768nog&symbol=' + search)
+                .then(res => {
+                    setCompany(res.data)
+                    console.log(res.data)
+                })
+                .catch(err => console.log(err))
         }
-
+        
     }
+
 
     return (
         <>
@@ -38,17 +47,17 @@ function MainPage() {
                 <div className="row2">
                     <h2>Company search</h2>
                     <div className="search">
+                       {isSearchValid === false ? '' : <div className="error" style={{color: "red"}}>{errorMessage}</div>}
                         <input type='text'
                             placeholder='Enter company name'
-                            value={search} 
-                            onChange={e => setSearch(e.target.value)}></input>
+                            value={search}
+                            onChange={e => setSearch(e.target.value)}
+                            onKeyPress={(e) => e.key === 'Enter'}></input>
                         <button onClick={searchCompany}>Search</button>
                     </div>
-                    
                 </div>
-
             </div>
-            <div className="container" >
+            <div className="container">
                 <Card company={company}></Card>
             </div>
         </>
