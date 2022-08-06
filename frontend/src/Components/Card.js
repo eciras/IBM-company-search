a16afa4e2da41ceb79da2a262a71108974f6f0e8
eciras <92270701+eciras@users.noreply.github.com>
2022-08-06 15:38:54 +0300

    Error message added

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -1,41 +1,53 @@
 import React from "react";
-import 'react-date-range/dist/styles.css'; 
-import 'react-date-range/dist/theme/default.css'; 
-import { DateRange } from 'react-date-range';
-import { useState } from 'react';
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import axios from "axios";
 
+function Card({ company, search }) {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
 
-function Card({ company }) {
+  const [stockData, setStockData] = useState("");
 
+  const searchStocks = (e) => {
+    axios.get("https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1631022248&to=1631627048&token=")
+    .then((res) => {
+        setStockData(res.data);
+      console.log(res.data);
+    })
+    .catch((err) => console.log(err));
+  }
 
-    const [state, setState] = useState([
-        {
-            startDate: new Date(),
-            endDate: null,
-            key: 'selection'
-        }
-    ]);
+  return (
+    <div className="card">
+      <div className="companyInfo">
+        <label onClick={searchStocks} className="pointer">{company.name}</label>
+        <label>{company.weburl}</label>
+        <br />
+        <label>{company.country}</label>      
+        <label>{company.currency}</label>
+     
+        
+      </div>
 
-
-    return (
-        <div className="card">
-            <label>{company.name}</label>
-            <label>{company.country}</label>
-            <label>{company.currency}</label>
-            <label>{company.weburl}</label>
-
-
-            <DateRange className="datePicker"
-                editableDateInputs={true}
-                onChange={item => setState([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={state}
-            />
-
-
-
-        </div>
-    )
+      <div>
+        <DateRange
+          className="datePicker"
+          editableDateInputs={true}
+          onChange={(item) => setDate([item.selection])}
+          moveRangeOnFirstSelection={false}
+          ranges={date}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -1,67 +1,67 @@
 import React from "react";
-import Card from './Card'
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import Card from "./Card";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function isSearchValid(search) {
-    return /^[a-zA-Z ]{1,35}$/g.test(search)
+  return /^[a-zA-Z ]{1,35}$/g.test(search);
 }
 
 function MainPage() {
+  const [search, setSearch] = useState("");
+  const [company, setCompany] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const [search, setSearch] = useState('');
-    const [company, setCompany] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-  
-
-    // const handleClick = () => {
-    //     if (!isSearchValid(search) === true) {
-    //     setErrorMessage("Example error message!")
-    //   }
-    // }
-
-    const searchCompany = (e) => {
-        if (!isSearchValid(search) === true) {
-            setErrorMessage("Example error message!");
-                
-            
-
-        } else {
-            axios.get('https://finnhub.io/api/v1/stock/profile2?token=cbkj62iad3i8o8768nog&symbol=' + search)
-                .then(res => {
-                    setCompany(res.data)
-                    console.log(res.data)
-                })
-                .catch(err => console.log(err))
-        }
-        
+  const searchCompany = (e) => {
+    e.preventDefault();
+    if (isSearchValid(search) === true) {
+      axios
+        .get(
+          "https://finnhub.io/api/v1/stock/profile2?token=cbkj62iad3i8o8768nog&symbol=" +
+            search
+        )
+        .then((res) => {
+          setCompany(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setErrorMessage(
+        "Company name must be letter only and less than 35 symbols!"
+      );
     }
+  };
 
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+          <h1>Find more about your Company</h1>
+        </div>
+        <div className="row2">
+          <h2>Search engine</h2>
+          <div className="search">
+            <div
+              className={isSearchValid(search) === true ? "noError" : "error"}
+            >
+              {errorMessage}
+            </div>
 
-    return (
-        <>
-            <div className="header">
-                <div className="row1">
-                    <h1>Find your Comapny</h1>
-                </div>
-                <div className="row2">
-                    <h2>Company search</h2>
-                    <div className="search">
-                       {isSearchValid === false ? '' : <div className="error" style={{color: "red"}}>{errorMessage}</div>}
-                        <input type='text'
-                            placeholder='Enter company name'
-                            value={search}
-                            onChange={e => setSearch(e.target.value)}
-                            onKeyPress={(e) => e.key === 'Enter'}></input>
-                        <button onClick={searchCompany}>Search</button>
-                    </div>
-                </div>
-            </div>
-            <div className="container">
-                <Card company={company}></Card>
-            </div>
-        </>
-    )
+            <input
+              type="text"
+              placeholder="Enter company name"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            ></input>
+            <button onClick={searchCompany}>Search</button>
+          </div>
+          <div className="container">
+            <Card company={company}></Card>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MainPage;
