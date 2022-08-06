cf2d749aa5b4e325bde529367655fb9ba906c22a
eciras <92270701+eciras@users.noreply.github.com>
2022-08-06 16:48:00 +0300

    Working stock candles

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -4,8 +4,9 @@ import "react-date-range/dist/theme/default.css";
 import { DateRange } from "react-date-range";
 import { useState } from "react";
 import axios from "axios";
+import Plot from "react-plotly.js";
 
-function Card({ company, search }) {
+function Card({ company }) {
   const [date, setDate] = useState([
     {
       startDate: new Date(),
@@ -14,27 +15,34 @@ function Card({ company, search }) {
     },
   ]);
 
-  const [stockData, setStockData] = useState("");
+  const [stockData, setStockData] = useState({});
+  const dateFormat = {year: '2-digit', month: '2-digit', day: '2-digit'};
+
 
   const searchStocks = (e) => {
-    axios.get("https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1631022248&to=1631627048&token=")
-    .then((res) => {
+    axios
+      .get(
+        "https://finnhub.io/api/v1/stock/candle?symbol=" +
+          company.ticker +
+          "&resolution=1&from=1631022248&to=1631627048&token=cbkj62iad3i8o8768nog"
+      )
+      .then((res) => {
         setStockData(res.data);
-      console.log(res.data);
-    })
-    .catch((err) => console.log(err));
-  }
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="card">
       <div className="companyInfo">
-        <label onClick={searchStocks} className="pointer">{company.name}</label>
+        <label onClick={searchStocks} className="pointer">
+          {company.name}
+        </label>
         <label>{company.weburl}</label>
         <br />
-        <label>{company.country}</label>      
+        <label>{company.country}</label>
         <label>{company.currency}</label>
-     
-        
       </div>
 
       <div>
@@ -44,6 +52,19 @@ function Card({ company, search }) {
           onChange={(item) => setDate([item.selection])}
           moveRangeOnFirstSelection={false}
           ranges={date}
+        />
+
+        <Plot
+          data={[
+            {
+              x: stockData.t == null ? [] : stockData.t.map(unix => Intl.DateTimeFormat('en-US', dateFormat).format(unix*1000)),
+              y: stockData.h,
+              type: "scatter",
+              mode: "lines+markers",
+              marker: { color: "red" },
+            }
+          ]}
+          layout={{ width: 620, height: 440, title: "A Fancy Plot" }}
         />
       </div>
     </div>
